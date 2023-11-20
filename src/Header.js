6f4df6c10bbb7e906f414ebd16b685e26ed33f7c
Karan Sharma <sharmakaran0212@gmail.com>
2023-11-20 18:55:52 +0200

    Completed Week 7 Lab 4

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to React Lab</h1>
+      <h2>Hi, This is Karan!!!</h2>
+    </header>
+  );
+}
+
+export default Header;
